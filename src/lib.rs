dfe50c87d74b30860823a09690e7a0061c4d4237
Stephan Hügel <urschrei@gmail.com>
2016-08-03 17:02:21 +0200

    Compiling, but overflowing

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,32 @@ pub fn point_line_distance(point: &(f64, f64), start: &(f64, f64), end: &(f64, f
     }
 }
 
+// Ramer–Douglas-Peucker line simplification algorithm
+// Test with an epsilon of 1.0
+pub fn rdp(points: &[(f64, f64)], epsilon: &f64) -> Vec<(f64, f64)> {
+    let mut dmax = 1.0;
+    let mut index: usize = 0;
+    for i in 1..points.len() - 1 {
+        let distance = point_line_distance(&points[i],
+                                           &points.first().unwrap(),
+                                           &points.last().unwrap());
+        if distance > dmax {
+            index = i;
+            dmax = distance;
+        }
+    }
+    if dmax >= *epsilon {
+        let mut first: Vec<(f64, f64)> = rdp(&points[..index + 1], &epsilon);
+        // remove last element
+        first.pop();
+        first.extend_from_slice(&rdp(&points[index..], &epsilon));
+        first
+    } else {
+        let other = vec![*points.first().unwrap(), *points.last().unwrap()];
+        other
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -36,5 +62,12 @@ mod tests {
         let end = (3.0, 4.0);
         assert_eq!(point_line_distance(&point, &start, &end),
                    0.7071067811865475);
+    }
+
+    #[test]
+    fn test_rdp() {
+        let foo: Vec<_> = rdp(&[(0.0, 0.0), (10.0, 10.0), (11.0, 11.0), (11.2, 13.45)],
+                              &1.0);
+        assert_eq!(foo, vec![(1.0, 2.0), (3.0, 4.0)]);
     }
 }
