7d067b7b6f7b7a372ec05deabb8db5e112819b1b
Stephan Hügel <urschrei@gmail.com>
2017-01-16 21:48:06 +0000

    Add longer linestring to benchmark

--- benches/benches.rs
+++ benches/benches.rs
@@ -6,7 +6,7 @@ extern crate rdp;
 extern crate geo;
 use geo::{LineString, Point};
 use geo::simplify::Simplify;
-use rdp::SimplifyVW;
+use geo::simplifyvw::SimplifyVW;
 
 #[bench]
 fn bench_rdp(b: &mut Bencher) {
@@ -21,6 +21,26 @@ fn bench_rdp(b: &mut Bencher) {
 #[bench]
 fn bench_visvalingam(b: &mut Bencher) {
     let points = include!("../src/mk_route.rs");
+    let points_ls: Vec<_> = points.iter().map(|e| Point::new(e[0], e[1])).collect();
+    let ls = LineString(points_ls);
+    b.iter(||{
+        ls.simplifyvw(&0.0000075);
+    });
+}
+
+#[bench]
+fn bench_rdp_long(b: &mut Bencher) {
+    let points = include!("../src/mk_route_long.rs");
+    let points_ls: Vec<_> = points.iter().map(|e| Point::new(e[0], e[1])).collect();
+    let ls = LineString(points_ls);
+    b.iter(||{
+        ls.simplify(&0.001);
+    });
+}
+
+#[bench]
+fn bench_visvalingam_long(b: &mut Bencher) {
+    let points = include!("../src/mk_route_long.rs");
     let points_ls: Vec<_> = points.iter().map(|e| Point::new(e[0], e[1])).collect();
     let ls = LineString(points_ls);
     b.iter(||{
