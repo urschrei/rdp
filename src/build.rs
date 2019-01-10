302088dcb3a8b7a82357c3b106c8a800eb78c333
Stephan Hügel <shugel@tcd.ie>
2019-01-10 15:53:16 +0000

    Switch to cbindgen, always generate header

--- src/build.rs
+++ src/build.rs
@@ -1,6 +1,12 @@
-extern crate cheddar;
+extern crate cbindgen;
+
+use std::env;
 
 fn main() {
-    cheddar::Cheddar::new().expect("could not read manifest")
-        .run_build("include/header.h");
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let config = cbindgen::Config::from_file("cbindgen.toml").unwrap();
+
+    cbindgen::generate_with_config(&crate_dir, config)
+        .expect("Unable to generate bindings")
+        .write_to_file("include/header.h");
 }
