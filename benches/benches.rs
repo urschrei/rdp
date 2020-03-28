681df416e85a277ab95bb7e824c1b66f30242d5a
Stephan Hügel <shugel@tcd.ie>
2020-03-28 14:10:24 +0000

    Add index-function benchmarks

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,6 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use geo::simplify::Simplify;
-use geo::simplifyvw::{SimplifyVW, SimplifyVWPreserve};
+use geo::simplify::{Simplify, SimplifyIdx};
+use geo::simplifyvw::{SimplifyVW, SimplifyVwIdx, SimplifyVWPreserve};
 use geo_types::LineString;
 
 fn bench_rdp(c: &mut Criterion) {
@@ -9,6 +9,16 @@ fn bench_rdp(c: &mut Criterion) {
         let ls: LineString<f64> = points.into();
         b.iter(|| {
             ls.simplify(&0.001);
+        });
+    });
+}
+
+fn bench_rdp_idx(c: &mut Criterion) {
+    c.bench_function("bench_rdp_idx", |b| {
+        let points = include!("../src/mk_route.rs");
+        let ls: LineString<f64> = points.into();
+        b.iter(|| {
+            ls.simplify_idx(&0.001);
         });
     });
 }
@@ -23,6 +33,16 @@ fn bench_visvalingam(c: &mut Criterion) {
     });
 }
 
+fn bench_visvalingam_idx(c: &mut Criterion) {
+    c.bench_function("bench_visvalingam_idx", |b| {
+        let points = include!("../src/mk_route.rs");
+        let ls: LineString<f64> = points.into();
+        b.iter(|| {
+            ls.simplifyvw_idx(&0.0000075);
+        });
+    });
+}
+
 fn bench_rdp_long(c: &mut Criterion) {
     c.bench_function("bench_rdp_long", |b| {
         let points = include!("../src/mk_route_long.rs");
@@ -33,12 +53,32 @@ fn bench_rdp_long(c: &mut Criterion) {
     });
 }
 
+fn bench_rdp_long_idx(c: &mut Criterion) {
+    c.bench_function("bench_rdp_long_idx", |b| {
+        let points = include!("../src/mk_route_long.rs");
+        let ls: LineString<f64> = points.into();
+        b.iter(|| {
+            ls.simplify_idx(&0.001);
+        });
+    });
+}
+
 fn bench_visvalingam_long(c: &mut Criterion) {
     c.bench_function("bench_visvalingam_long", |b| {
         let points = include!("../src/mk_route_long.rs");
         let ls: LineString<f64> = points.into();
         b.iter(|| {
             ls.simplifyvw(&0.0000075);
+        });
+    });
+}
+
+fn bench_visvalingam_long_idx(c: &mut Criterion) {
+    c.bench_function("bench_visvalingam_long_idx", |b| {
+        let points = include!("../src/mk_route_long.rs");
+        let ls: LineString<f64> = points.into();
+        b.iter(|| {
+            ls.simplifyvw_idx(&0.0000075);
         });
     });
 }
@@ -56,9 +96,13 @@ fn bench_visvalingamp_long(c: &mut Criterion) {
 criterion_group!(
     benches,
     bench_rdp,
+    bench_rdp_idx,
     bench_visvalingam,
+    bench_visvalingam_idx,
     bench_rdp_long,
+    bench_rdp_long_idx,
     bench_visvalingam_long,
+    bench_visvalingam_long_idx,
     bench_visvalingamp_long
 );
 criterion_main!(benches);
